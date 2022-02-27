a58d0af042e52636a8a050c27b91c33fc1c77d1d
ࢪيفــٰآي <98587160+rify39@users.noreply.github.com>
2022-02-27 10:01:37 +0300

    Update roz.py

--- roz.py
+++ roz.py
@@ -7,7 +7,7 @@ from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.raw.functions.messages import UpdatePinnedMessage
 
 bot=Client(
-    "kdthon bot",
+    "jmthon bot",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
